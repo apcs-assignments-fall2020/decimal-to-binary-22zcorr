9dd0ce7c24fcc3fccd6695d9bb2607c17ffd8970
22zcorr <22zcorr@athenian.org>
2020-09-16 03:58:45 +0000

    I wrote the code and am going to try to submit it right this time

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,25 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+    int input=Integer.parseInt(binary);
+    
+    int output =0;
+    for(double i=0;input>0;i++){
+        int mult = (int) Math.pow(2.0,i);
+        output=output+((input%10)*(mult));
+        input=(input/10);
+        
+    }
+    return output;
+
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type a number in Binary (1's and 0's)");
+        String binNum = scan.nextLine();
+        System.out.println("This number in Decimal is " +binaryToDecimal(binNum));
+        scan.close();
     }
 }
